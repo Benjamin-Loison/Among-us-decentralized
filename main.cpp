809a11f1a53e762075ee59119b849c16902a2236
Benjamin Loison <benjamin.loison@orange.fr>
2021-10-15 17:18:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@
 #include <QMediaPlayer>
 #include "main.h"
 #include "QLabelKeys.h"
+#include "Server.h"
+#include "Client.h"
 
 QMediaPlayer* player;
 QLabelKeys* qLabelKeys;
 InGameGUI currentInGameGUI = IN_GAME_GUI_NONE;
+Server* server;
+Client* client;
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +27,18 @@ int main(int argc, char *argv[])
             qInfo("languageFile couldn't be loaded !");*/
     // QSettings like TravianBlockchained
     player = new QMediaPlayer;
+
+    bool runServer = false,
+         runClient = true;
+    if(runServer)
+    {
+        server = new Server;
+    }
+    if(runClient)
+    {
+        client = new Client;
+    }
+
     qLabelKeys = new QLabelKeys;
     qLabelKeys->installEventFilter(qLabelKeys);
 
