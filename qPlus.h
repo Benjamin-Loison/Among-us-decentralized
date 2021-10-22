46cfd22a82b9c99ffeba28e7fabfc33e374818f3
Benjamin Loison <benjamin.loison@orange.fr>
2021-10-22 16:04:45 +0200

    Update qPlus.h

--- qPlus.h
+++ qPlus.h
@@ -9,5 +9,6 @@ double distance(quint16 x0, quint16 y0, quint16 x1, quint16 y1);
 extern QString assetsFolder;
 QPixmap* getQPixmap(QString filePath);
 void playSound(QString soundFile);
+QString getText(QString title, QString label);
 
 #endif
