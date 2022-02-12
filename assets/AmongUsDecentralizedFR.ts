885ae85b2d6fdd50e1c2d26827967fe36601b24f
Benjamin Loison <benjamin.loison@orange.fr>
2022-02-12 20:19:10 +0100

    Updated french translations for 1.0.1

--- assets/AmongUsDecentralizedFR.ts
+++ assets/AmongUsDecentralizedFR.ts
@@ -4,107 +4,107 @@
 <context>
     <name>InGameUI</name>
     <message>
-        <location filename="InGameUI.cpp" line="26"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="26"/>
         <source>Among Us decentralized</source>
         <translation>Among Us décentralisé</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="104"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="111"/>
         <source>Upper Engine</source>
         <translation>Moteur supérieur</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="105"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="112"/>
         <source>MedBay</source>
         <translation>Infirmerie</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="106"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="113"/>
         <source>Cafeteria</source>
         <translation>Cafétéria</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="107"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="114"/>
         <source>Weapons</source>
         <translation>Armes</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="108"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="115"/>
         <source>Reactor</source>
         <translation>Réacteur</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="109"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="116"/>
         <source>Security</source>
         <translation>Sécurité</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="110"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="117"/>
         <source>Admin</source>
         <translation>Administration</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="111"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="118"/>
         <source>O2</source>
         <translation>O2</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="112"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="119"/>
         <source>Navigation</source>
         <translation>Navigation</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="113"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="120"/>
         <source>Lower Engine</source>
         <translation>Moteur inférieur</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="114"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="121"/>
         <source>Electrical</source>
         <translation>Électricité</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="115"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="122"/>
         <source>Storage</source>
         <translation>Cargo</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="116"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="123"/>
         <source>Communications</source>
         <translation>Radio</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="117"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="124"/>
         <source>Shields</source>
         <translation>Boucliers</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="509"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="531"/>
         <source>crewmates</source>
         <translation>équipiers</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="509"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="531"/>
         <source>impostors</source>
         <translation>imposteurs</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="511"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="533"/>
         <source>%1&apos; victory</source>
         <translation>victoire des %1</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="596"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="618"/>
         <source>You are an Impostor !</source>
         <translation>Vous êtes un imposteur !</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="644"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="667"/>
         <source>Ready</source>
         <translation>Prêt</translation>
     </message>
     <message>
-        <location filename="InGameUI.cpp" line="672"/>
+        <location filename="../sources/ui/InGameUI.cpp" line="696"/>
         <source>Waiting for other players</source>
         <translation>En attente des autres joueurs</translation>
     </message>
@@ -112,62 +112,62 @@
 <context>
     <name>MeetingResultsUI</name>
     <message>
-        <location filename="meetings.cpp" line="70"/>
+        <location filename="../sources/guis/meetings.cpp" line="70"/>
         <source>Voting results</source>
         <translation>Résultats du vote</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="97"/>
+        <location filename="../sources/guis/meetings.cpp" line="97"/>
         <source>Skip vote</source>
         <translation>Passer le vote</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="105"/>
+        <location filename="../sources/guis/meetings.cpp" line="105"/>
         <source>Majority voted to skip vote. No player was killed.</source>
         <translation>La majorité a voté pour passer le vote. Aucun joueur n&apos;a été tué.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="126"/>
+        <location filename="../sources/guis/meetings.cpp" line="126"/>
         <source>%1 got the most votes and was killed.</source>
         <translation>%1 a obtenu le plus de votes et a été tué.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="128"/>
+        <location filename="../sources/guis/meetings.cpp" line="128"/>
         <source>He was an Impostor.</source>
         <translation>C&apos;était un imposteur.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="140"/>
+        <location filename="../sources/guis/meetings.cpp" line="140"/>
         <source>%1 Impostor left.</source>
         <translation>%1 imposteur restant.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="142"/>
+        <location filename="../sources/guis/meetings.cpp" line="142"/>
         <source>%1 Impostors left.</source>
         <translation>%1 imposteurs restants.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="132"/>
+        <location filename="../sources/guis/meetings.cpp" line="132"/>
         <source>He was not an Impostor.</source>
         <translation>Ce n&apos;était pas un imposteur.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="136"/>
+        <location filename="../sources/guis/meetings.cpp" line="136"/>
         <source>A tie occurred. No player was killed.</source>
         <translation>Ex-aequo. Aucun joueur n&apos;a été tué.</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="147"/>
+        <location filename="../sources/guis/meetings.cpp" line="147"/>
         <source>Proceed</source>
         <translation>Continuer</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="159"/>
+        <location filename="../sources/guis/meetings.cpp" line="159"/>
         <source>Waiting for one other player...</source>
         <translation>En attente d&apos;un autre joueur...</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="161"/>
+        <location filename="../sources/guis/meetings.cpp" line="161"/>
         <source>Waiting for %1 other players...</source>
         <translation>En attente de %1 autres joueurs...</translation>
     </message>
@@ -175,32 +175,32 @@
 <context>
     <name>MeetingUI</name>
     <message>
-        <location filename="meetings.cpp" line="10"/>
+        <location filename="../sources/guis/meetings.cpp" line="10"/>
         <source>Who is the Impostor ?</source>
         <translation>Qui est l&apos;imposteur ?</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="13"/>
+        <location filename="../sources/guis/meetings.cpp" line="13"/>
         <source>%1&apos;s body was found by %2</source>
         <translation>Le cadavre de %1 a été trouvé par %2</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="15"/>
+        <location filename="../sources/guis/meetings.cpp" line="15"/>
         <source>Emergency meeting requested by %1</source>
         <translation>Réunion d&apos;urgence réclamée par %1</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="24"/>
+        <location filename="../sources/guis/meetings.cpp" line="24"/>
         <source>%1 (dead)</source>
         <translation>%1 (mort)</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="39"/>
+        <location filename="../sources/guis/meetings.cpp" line="39"/>
         <source>Skip vote</source>
         <translation>Passer le vote</translation>
     </message>
     <message>
-        <location filename="meetings.cpp" line="58"/>
+        <location filename="../sources/guis/meetings.cpp" line="58"/>
         <source>Waiting for other players...</source>
         <translation>En attente des autres joueurs...</translation>
     </message>
@@ -208,58 +208,83 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="asteroids.cpp" line="119"/>
+        <location filename="../sources/tasks/asteroids.cpp" line="118"/>
         <source>%1 left</source>
         <translation>%1 restant(s)</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="55"/>
+        <location filename="../sources/main.cpp" line="61"/>
         <source>First to run</source>
         <translation>Premier à rejoindre la partie</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="55"/>
+        <location filename="../sources/main.cpp" line="61"/>
         <source>Are you the first to run for this game ?</source>
         <translation>Êtes-vous le premier à rejoindre la partie ?</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="70"/>
+        <location filename="../sources/main.cpp" line="80"/>
         <source>Peer address</source>
         <translation>Adresse d&apos;un participant</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="70"/>
+        <location filename="../sources/main.cpp" line="80"/>
         <source>A peer address</source>
         <translation>L&apos;adresse d&apos;un participant</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="104"/>
-        <location filename="main.cpp" line="121"/>
+        <location filename="../sources/main.cpp" line="95"/>
+        <source>Map choice</source>
+        <translation>Choix de la carte</translation>
+    </message>
+    <message>
+        <location filename="../sources/main.cpp" line="95"/>
+        <source>What map do you want to play on ?</source>
+        <translation>Sur quelle carte souhaitez-vous jouer ?</translation>
+    </message>
+    <message>
+        <location filename="../sources/main.cpp" line="101"/>
+        <source>Autoconfiguration</source>
+        <translation>Configuration automatique</translation>
+    </message>
+    <message>
+        <location filename="../sources/main.cpp" line="101"/>
+        <source>Is your port %1 opened to others ?</source>
+        <translation>Est-ce que votre port %1 est ouvert aux autres ?</translation>
+    </message>
+    <message>
+        <location filename="../sources/main.cpp" line="125"/>
+        <source>IP and port to share to peers</source>
+        <translation>IP et port à partager aux autres</translation>
+    </message>
+    <message>
+        <location filename="../sources/main.cpp" line="156"/>
+        <location filename="../sources/main.cpp" line="179"/>
         <source>Nickname</source>
         <translation>Pseudonyme</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="104"/>
+        <location filename="../sources/main.cpp" line="156"/>
         <source>Your nickname</source>
         <translation>Votre pseudonyme</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="121"/>
+        <location filename="../sources/main.cpp" line="179"/>
         <source>This nickname is already used!</source>
         <translation>Ce pseudonyme est déjà utilisé !</translation>
     </message>
     <message>
-        <location filename="vitals.cpp" line="40"/>
+        <location filename="../sources/guis/vitals.cpp" line="40"/>
         <source>DEAD</source>
         <translation>MORT</translation>
     </message>
     <message>
-        <location filename="vitals.cpp" line="40"/>
+        <location filename="../sources/guis/vitals.cpp" line="40"/>
         <source>D/C</source>
         <translation>D/C</translation>
     </message>
     <message>
-        <location filename="vitals.cpp" line="40"/>
+        <location filename="../sources/guis/vitals.cpp" line="40"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
